f6eb33708d2f3e4a41a5b3cb48e56de002d3a292
Precious Kanu <precious.a.kanu@Preciouss-MacBook-Pro.local>
2022-03-05 10:34:44 +0100

    first commit

---
+++ src/components/Card/CardStyles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+// Styled Component
+export const CardStyle = styled.div`
+  font-size: 1.5em;
+  text-align: center;
+  color: blue;
+`;
---
+++ src/components/Card/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { CardStyle } from "./CardStyles";
+
+const Card = () => {
+  return (
+    <>
+      <CardStyle>Samuel Jackson</CardStyle>
+    </>
+  );
+};
+
+export default Card;
